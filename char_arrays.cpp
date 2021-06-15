d579eca0cfb53c1635641587a8fcb4136b336c59
murlidharpathakprogrammer <murlidharpathakprogrammer@gmail.com>
2021-06-15 23:46:54 +0530

    Add char array with and without '\0' for garbage collector cpp file

---
+++ char_arrays.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char cArray[]={'a','b','c','\0'};
+    char chArray[]={'f','g','h','\0'};
+    char charArray[]="fgh";
+
+    cout<<chArray<<"\n";
+    cout<<charArray<<"\n";
+
+    return 0;
+}
